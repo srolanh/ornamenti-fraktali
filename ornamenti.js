e4aaf1c5eb6456114912a5b92d2f37b7a4e4d373
srolanh <dz.melgalvis@gmail.com>
2015-01-02 15:01:27 +0200

    add drawing function

--- ornamenti.js
+++ ornamenti.js
@@ -1,7 +1,27 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
-rWidth = 10;
+function drawImage(ctx, image, rWidth, xWidth, yHeight) {
+	var color0 = "#FFFFFF";
+	var color1 = "#0000FF";
+	var widthPersistent =xWidth;
+	var i;
+	var j;
+	for (i = 0; i < image.length; i++) {
+		for (j = 0; j < image[i].length; j++) {
+			if (image[i][j] == 1) {
+				ctx.fillStyle = color1;
+			} else {
+				ctx.fillStyle = color0;
+			}
+			ctx.fillRect(xWidth, yHeight, rWidth, rWidth);
+			xWidth += rWidth;
+		}
+		xWidth = widthPersistent;
+		yHeight += rWidth;
+	}
+}
 
-ctx.fillStyle = "#0000FF";
-ctx.fillRect(0, 0, rWidth, rWidth);
+i = [[0,1,0],[1,0,1],[0,1,0]];
+
+drawImage(ctx, i, 10, 0, 0);
